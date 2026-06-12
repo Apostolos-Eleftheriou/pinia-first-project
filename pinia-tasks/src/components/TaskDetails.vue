42dfb807bd7265635763c47535abf2871dd74bff
Apostolos Eleftheriou <eleftheriou.apostolis@gmail.com>
2026-06-12 22:55:25 +0300

    add on enter and on esc handling and automatic focus of input on edit

--- pinia-tasks/src/components/TaskDetails.vue
+++ pinia-tasks/src/components/TaskDetails.vue
@@ -4,10 +4,10 @@
             task.title }}
         </h3>
         <input v-if="editTask" type="text" class="border-2 border-gray-300 rounded-lg p-2 w-full col-span-2"
-            :value="title" @change="(e) => title = e.target.value" />
+            :value="title" @change="(e) => title = e.target.value" ref="titleInput"
+            @keydown.enter="(e) => editAndHide(task.id, e.target.value)" @keydown.esc="editTask = false" />
         <div class="icons text-end w-full flex items-center justify-end gap-2 select-none" v-if="!editTask">
-            <i class="material-icons cursor-pointer hover:text-red-800 duration-100"
-                @click="editTask = true; title = task.title">edit</i>
+            <i class="material-icons cursor-pointer hover:text-red-800 duration-100" @click="startEdit()">edit</i>
             <i class="material-icons cursor-pointer hover:text-red-800 duration-100"
                 @click="taskStore.removeTask(task.id)">delete</i>
             <i class="material-icons cursor-pointer hover:text-rose-400 duration-100" :class="{ isFav: task.completed }"
@@ -22,14 +22,23 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, nextTick } from 'vue'
 import { useTaskStore } from '../stores/TaskStore'
 
 const taskStore = useTaskStore()
 
 const props = defineProps(['task'])
+const titleInput = ref(null)
 const title = ref(props.task.title)
 const editTask = ref(false)
+
+const startEdit = async () => {
+    editTask.value = true
+    title.value = props.task.title
+    await nextTick()
+    titleInput.value?.focus()
+}
+
 const editAndHide = (taskId, taskTitle) => {
     if (taskTitle !== '') taskStore.editTask(taskId, taskTitle);
     editTask.value = false;
